06ff2bcb3fece4d3e7f8e39d413999b5dd9b5711
Marcos200Devop <82180545+Marcos200Devop@users.noreply.github.com>
2022-10-23 15:05:23 -0600

    inicial page

---
+++ js/index.js
@@ -0,0 +1,27 @@
+'use strict';
+
+const documentReady = () => {
+  const heroTitle = document.querySelector('.hero__title');
+  const heroButtonSi = document.getElementById('heroButtonSi');
+  const heroButtonNo = document.querySelector('#heroButtonNo');
+
+  const nowZomosNobios = () => {
+    alert('💞 AHORA, YA SABES QUIEN ME GUSTA💞');
+    alert('😱QUE EMOCIONNNNNN!!!😱');
+    location.href = 'https://www.youtube.com/watch?v=arj6eT77hzE&ab_channel=Corridosmusica';
+  };
+
+  const evitarKCRompanMyBobo = () => {
+    heroButtonNo.style.position = 'absolute';
+    heroButtonNo.style.top = (Math.random() * window.innerHeight) + 'px';
+    heroButtonNo.style.left = (Math.random() * window.innerWidth) + 'px';
+  };
+
+  const partner = prompt('🥰 ESCRIBI TU NOMBREEEEEEE??? 🥰');
+  heroTitle.innerHTML += partner + ' ❤';
+
+  heroButtonSi.addEventListener('click', nowZomosNobios);
+  heroButtonNo.addEventListener('mouseover', evitarKCRompanMyBobo);
+};
+
+document.addEventListener('DOMContentLoaded', documentReady);
